2fecc9a1254a31891867f73435ed6405c15ff269
Simranjit Singh <theliongitlab@gmail.com>
2022-05-17 02:27:57 +0530

    set up theme provider

---
+++ client/src/Layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='spinner-backdrop'>
+      <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
